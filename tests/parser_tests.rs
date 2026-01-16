9f2331fd67a04d2aa68dcab3d215f41f99e992b0
Harin <iamharinramesh@gmail.com>
2026-01-16 22:46:18 +0530

    added tests

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -15,7 +15,15 @@ fn parse_integer_literal() {
 fn parse_negative_integer() {
     let mut parser = Parser::new("-42;");
     let expr = parser.parse().unwrap();
-    assert_eq!(expr, Expr::Block(vec![Expr::Int(-42)]));
+    // Unary minus is represented as (0 - 42)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(0)),
+            Box::new(Expr::Int(42))
+        )])
+    );
 }
 
 #[test]
@@ -50,7 +58,15 @@ fn parse_float_literal() {
 fn parse_negative_float() {
     let mut parser = Parser::new("-3.14;");
     let expr = parser.parse().unwrap();
-    assert_eq!(expr, Expr::Block(vec![Expr::Float(-3.14)]));
+    // Unary minus is represented as (0 - 3.14)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(0)),
+            Box::new(Expr::Float(3.14))
+        )])
+    );
 }
 
 #[test]
@@ -622,4 +638,614 @@ fn eval_float_addition() {
     let axe = Axe::new();
     let result = axe.eval(expr).unwrap();
     assert_eq!(result, Value::Float(4.0));
+}
+
+// =============================================================================
+// Subtraction Expression Tests
+// =============================================================================
+
+#[test]
+fn parse_simple_subtraction() {
+    let mut parser = Parser::new("5 - 3;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(5)),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+#[test]
+fn parse_chained_subtraction() {
+    let mut parser = Parser::new("10 - 3 - 2;");
+    let expr = parser.parse().unwrap();
+    // Left-associative: (10 - 3) - 2
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Binary(
+                Operation::Sub,
+                Box::new(Expr::Int(10)),
+                Box::new(Expr::Int(3))
+            )),
+            Box::new(Expr::Int(2))
+        )])
+    );
+}
+
+#[test]
+fn parse_subtraction_with_parentheses() {
+    let mut parser = Parser::new("10 - (3 - 2);");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(10)),
+            Box::new(Expr::Binary(
+                Operation::Sub,
+                Box::new(Expr::Int(3)),
+                Box::new(Expr::Int(2))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_mixed_addition_subtraction() {
+    let mut parser = Parser::new("1 + 2 - 3;");
+    let expr = parser.parse().unwrap();
+    // Left-associative: (1 + 2) - 3
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Binary(
+                Operation::Add,
+                Box::new(Expr::Int(1)),
+                Box::new(Expr::Int(2))
+            )),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+#[test]
+fn parse_subtraction_no_spaces() {
+    let mut parser = Parser::new("5-3;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(5)),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+// =============================================================================
+// Multiplication Expression Tests
+// =============================================================================
+
+#[test]
+fn parse_simple_multiplication() {
+    let mut parser = Parser::new("3 * 4;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Int(3)),
+            Box::new(Expr::Int(4))
+        )])
+    );
+}
+
+#[test]
+fn parse_chained_multiplication() {
+    let mut parser = Parser::new("2 * 3 * 4;");
+    let expr = parser.parse().unwrap();
+    // Left-associative: (2 * 3) * 4
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Binary(
+                Operation::Mul,
+                Box::new(Expr::Int(2)),
+                Box::new(Expr::Int(3))
+            )),
+            Box::new(Expr::Int(4))
+        )])
+    );
+}
+
+#[test]
+fn parse_multiplication_with_parentheses() {
+    let mut parser = Parser::new("2 * (3 * 4);");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Int(2)),
+            Box::new(Expr::Binary(
+                Operation::Mul,
+                Box::new(Expr::Int(3)),
+                Box::new(Expr::Int(4))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_multiplication_no_spaces() {
+    let mut parser = Parser::new("3*4;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Int(3)),
+            Box::new(Expr::Int(4))
+        )])
+    );
+}
+
+#[test]
+fn parse_multiplication_with_floats() {
+    let mut parser = Parser::new("2.5 * 4.0;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Float(2.5)),
+            Box::new(Expr::Float(4.0))
+        )])
+    );
+}
+
+// =============================================================================
+// Division Expression Tests
+// =============================================================================
+
+#[test]
+fn parse_simple_division() {
+    let mut parser = Parser::new("10 / 2;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Div,
+            Box::new(Expr::Int(10)),
+            Box::new(Expr::Int(2))
+        )])
+    );
+}
+
+#[test]
+fn parse_chained_division() {
+    let mut parser = Parser::new("100 / 10 / 2;");
+    let expr = parser.parse().unwrap();
+    // Left-associative: (100 / 10) / 2
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Div,
+            Box::new(Expr::Binary(
+                Operation::Div,
+                Box::new(Expr::Int(100)),
+                Box::new(Expr::Int(10))
+            )),
+            Box::new(Expr::Int(2))
+        )])
+    );
+}
+
+#[test]
+fn parse_division_with_parentheses() {
+    let mut parser = Parser::new("100 / (10 / 2);");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Div,
+            Box::new(Expr::Int(100)),
+            Box::new(Expr::Binary(
+                Operation::Div,
+                Box::new(Expr::Int(10)),
+                Box::new(Expr::Int(2))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_mixed_multiplication_division() {
+    let mut parser = Parser::new("2 * 6 / 3;");
+    let expr = parser.parse().unwrap();
+    // Left-associative: (2 * 6) / 3
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Div,
+            Box::new(Expr::Binary(
+                Operation::Mul,
+                Box::new(Expr::Int(2)),
+                Box::new(Expr::Int(6))
+            )),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+#[test]
+fn parse_division_no_spaces() {
+    let mut parser = Parser::new("10/2;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Div,
+            Box::new(Expr::Int(10)),
+            Box::new(Expr::Int(2))
+        )])
+    );
+}
+
+// =============================================================================
+// Operator Precedence Tests (Multiplicative binds tighter than Additive)
+// =============================================================================
+
+#[test]
+fn parse_precedence_mul_over_add() {
+    let mut parser = Parser::new("1 + 2 * 3;");
+    let expr = parser.parse().unwrap();
+    // Should parse as: 1 + (2 * 3)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Add,
+            Box::new(Expr::Int(1)),
+            Box::new(Expr::Binary(
+                Operation::Mul,
+                Box::new(Expr::Int(2)),
+                Box::new(Expr::Int(3))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_precedence_div_over_sub() {
+    let mut parser = Parser::new("10 - 6 / 2;");
+    let expr = parser.parse().unwrap();
+    // Should parse as: 10 - (6 / 2)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(10)),
+            Box::new(Expr::Binary(
+                Operation::Div,
+                Box::new(Expr::Int(6)),
+                Box::new(Expr::Int(2))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_precedence_mul_before_sub() {
+    let mut parser = Parser::new("2 * 3 - 1;");
+    let expr = parser.parse().unwrap();
+    // Should parse as: (2 * 3) - 1
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Binary(
+                Operation::Mul,
+                Box::new(Expr::Int(2)),
+                Box::new(Expr::Int(3))
+            )),
+            Box::new(Expr::Int(1))
+        )])
+    );
+}
+
+#[test]
+fn parse_precedence_override_with_parentheses() {
+    let mut parser = Parser::new("(1 + 2) * 3;");
+    let expr = parser.parse().unwrap();
+    // Parentheses override: (1 + 2) * 3
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Binary(
+                Operation::Add,
+                Box::new(Expr::Int(1)),
+                Box::new(Expr::Int(2))
+            )),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+#[test]
+fn parse_complex_precedence() {
+    let mut parser = Parser::new("1 + 2 * 3 - 4 / 2;");
+    let expr = parser.parse().unwrap();
+    // Should parse as: (1 + (2 * 3)) - (4 / 2)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Binary(
+                Operation::Add,
+                Box::new(Expr::Int(1)),
+                Box::new(Expr::Binary(
+                    Operation::Mul,
+                    Box::new(Expr::Int(2)),
+                    Box::new(Expr::Int(3))
+                ))
+            )),
+            Box::new(Expr::Binary(
+                Operation::Div,
+                Box::new(Expr::Int(4)),
+                Box::new(Expr::Int(2))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_all_operators_chained() {
+    let mut parser = Parser::new("10 + 5 - 3 * 2 / 1;");
+    let expr = parser.parse().unwrap();
+    // Should parse as: (10 + 5) - ((3 * 2) / 1)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Binary(
+                Operation::Add,
+                Box::new(Expr::Int(10)),
+                Box::new(Expr::Int(5))
+            )),
+            Box::new(Expr::Binary(
+                Operation::Div,
+                Box::new(Expr::Binary(
+                    Operation::Mul,
+                    Box::new(Expr::Int(3)),
+                    Box::new(Expr::Int(2))
+                )),
+                Box::new(Expr::Int(1))
+            ))
+        )])
+    );
+}
+
+// =============================================================================
+// Unary Operator Tests (parse_primary unary +/-)
+// =============================================================================
+
+#[test]
+fn parse_unary_minus_in_expression() {
+    let mut parser = Parser::new("5 + -3;");
+    let expr = parser.parse().unwrap();
+    // -3 is represented as (0 - 3)
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Add,
+            Box::new(Expr::Int(5)),
+            Box::new(Expr::Binary(
+                Operation::Sub,
+                Box::new(Expr::Int(0)),
+                Box::new(Expr::Int(3))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_unary_plus_in_expression() {
+    let mut parser = Parser::new("5 + +3;");
+    let expr = parser.parse().unwrap();
+    // +3 is just 3
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Add,
+            Box::new(Expr::Int(5)),
+            Box::new(Expr::Int(3))
+        )])
+    );
+}
+
+#[test]
+fn parse_double_unary_minus_with_parens() {
+    let mut parser = Parser::new("-(-5);");
+    let expr = parser.parse().unwrap();
+    // -(-5) is (0 - (0 - 5))
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(0)),
+            Box::new(Expr::Binary(
+                Operation::Sub,
+                Box::new(Expr::Int(0)),
+                Box::new(Expr::Int(5))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_unary_minus_with_parentheses() {
+    let mut parser = Parser::new("-(1 + 2);");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Sub,
+            Box::new(Expr::Int(0)),
+            Box::new(Expr::Binary(
+                Operation::Add,
+                Box::new(Expr::Int(1)),
+                Box::new(Expr::Int(2))
+            ))
+        )])
+    );
+}
+
+#[test]
+fn parse_unary_in_multiplicative() {
+    let mut parser = Parser::new("2 * -3;");
+    let expr = parser.parse().unwrap();
+    assert_eq!(
+        expr,
+        Expr::Block(vec![Expr::Binary(
+            Operation::Mul,
+            Box::new(Expr::Int(2)),
+            Box::new(Expr::Binary(
+                Operation::Sub,
+                Box::new(Expr::Int(0)),
+                Box::new(Expr::Int(3))
+            ))
+        )])
+    );
+}
+
+// =============================================================================
+// Evaluation Tests for Arithmetic Operations
+// =============================================================================
+
+#[test]
+fn eval_simple_subtraction() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("10 - 3;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(7));
+}
+
+#[test]
+fn eval_chained_subtraction() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("10 - 3 - 2;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(5)); // (10 - 3) - 2 = 5
+}
+
+#[test]
+fn eval_simple_multiplication() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("3 * 4;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(12));
+}
+
+#[test]
+fn eval_simple_division() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("10 / 2;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(5));
+}
+
+#[test]
+fn eval_precedence_mul_over_add() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("1 + 2 * 3;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(7)); // 1 + (2 * 3) = 7
+}
+
+#[test]
+fn eval_precedence_with_parentheses() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("(1 + 2) * 3;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(9)); // (1 + 2) * 3 = 9
+}
+
+#[test]
+fn eval_complex_expression() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("2 + 3 * 4 - 10 / 2;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(9)); // 2 + 12 - 5 = 9
+}
+
+#[test]
+fn eval_unary_minus_in_expression() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("5 + -3;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(2));
+}
+
+#[test]
+fn eval_unary_minus_with_multiplication() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("2 * -3;");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(-6));
+}
+
+#[test]
+fn eval_nested_parentheses_complex() {
+    use axe::{Axe, Value};
+
+    let mut parser = Parser::new("((2 + 3) * (4 - 1));");
+    let expr = parser.parse().unwrap();
+
+    let axe = Axe::new();
+    let result = axe.eval(expr).unwrap();
+    assert_eq!(result, Value::Int(15)); // (5) * (3) = 15
 }
